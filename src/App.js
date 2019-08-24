b494af102b43271f0232c12901f3eb3d38664734
imdeepmind <infinityatme@gmail.com>
2019-08-24 15:25:47 +0530

    added update code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import ReactResumableJs from 'react-resumable-js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return (
+      <div>
+          <p>You can add other inputs, selects or stuff right here to complete a form.</p>
+          <ReactResumableJs
+            uploaderID="image-upload"
+            dropTargetID="myDropTarget"
+            filetypes={["jpg", "JPG", "png", "PNG", "mp4", "MP4", "mkv", "deb"]}
+            maxFileSize={512000000}
+            fileAccept="*/*"
+            fileAddedMessage="Started!"
+            completedMessage="Complete!"
+            service="http://localhost:3000/upload"
+            textLabel="Uploaded files"
+            previousText="Drop to upload your media:"
+            disableDragAndDrop={true}
+            onFileSuccess={(file, message) => {
+              console.log(file, message);
+            }}
+            onFileAdded={(file, resumable) => {
+              console.log('added');
+              resumable.upload();
+            }}
+            maxFiles={1}
+            startButton={true}
+            pauseButton={true}
+            cancelButton={true}
+            onStartUpload={() => {
+                console.log("Start upload");
+            }}
+            onCancelUpload={() => {
+                this.inputDisable = false;
+            }}
+            onPauseUpload={() =>{
+                this.inputDisable = false;
+            }}
+            onResumeUpload={() => {
+                this.inputDisable = true;
+            }}
+          />
+      </div>
+    )
+  }
 }
 
 export default App;
