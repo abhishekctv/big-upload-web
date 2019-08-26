8106058067623fc63a723780ea298650afe1fffa
imdeepmind <infinityatme@gmail.com>
2019-08-26 13:14:21 +0530

    update

--- src/components/resumable/index.js
+++ src/components/resumable/index.js
@@ -286,7 +286,7 @@ export default class ReactResumableJs extends React.Component {
                 </div>
 
                 {fileList}
-                {startButton}
+                {/* {startButton} */}
                 {pauseButton}
                 {cancelButton}
 
